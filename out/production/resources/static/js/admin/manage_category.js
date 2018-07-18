8d654a2d6d9666cab05dc925c951e1c7373e882e
unknown <kenkenls95@gmail.com>
2018-07-18 23:00:29 +0700

    done

--- out/production/resources/static/js/admin/manage_category.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#table-categoty').dataTable()
-    $('.btn-detail').on('click',function () {
-        var id = $(this).data("id")
-        axios.get("/api/category/detail/"+id).then(function (res) {
-
-        })
-    })
-})
--- out/production/resources/static/js/format_money.js
+++
@@ -1,8 +0,0 @@
-function format(x) {
-    x = x.toLocaleString('vi', {style : 'currency', currency : 'VND'});
-    return x
-}
-$('.price').each(function () {
-    $(this).text(format(parseInt($(this).text())))
-})
-// console.log($('.price'))
